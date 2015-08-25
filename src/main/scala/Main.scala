88f886c89bb3be1caf61cfd9d0fe39605b7438c0
Justin Gallardo <justin.gallardo@gmail.com>
2015-08-24 23:13:29 -0700

    Use loop to publish a lot of messages.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,8 @@ import akka.actor.{PoisonPill, Props, ActorSystem}
 import in.jirw.pubsub.{SubActor, PubSubActor}
 import in.jirw.pubsub.PubSubActor.{Unsubscribe, PublishAll, Publish, Subscribe}
 
+import scala.util.Random
+
 /**
  * Created by jirwin on 8/24/15.
  */
@@ -15,20 +17,14 @@ object Main extends App {
   val subActor1 = system.actorOf(SubActor.props(publish), name = "subactor-1")
   val subActor2 = system.actorOf(SubActor.props(publish2), name = "subactor-2")
 
+  val topics = List("jirwin", "foo", "bar", "christina")
 
   pubSubActor ! Subscribe("jirwin", subActor1)
   pubSubActor ! Subscribe("jirwin", subActor2)
   pubSubActor ! Subscribe("christina", subActor2)
 
-  pubSubActor ! Publish("jirwin", "This is to topic jirwin")
-  pubSubActor ! Publish("christina", "This is to topic christina")
-  pubSubActor ! Publish("foo", "This is to the empty topic foo")
-
-  pubSubActor ! PublishAll("This is a message publshed to all topics")
-  pubSubActor ! PublishAll("This is another message published to all topics")
-
-  subActor1 ! PoisonPill
-
-  pubSubActor ! Publish("jirwin", "This is a message after subActor1 was killed.")
-
+  while(true) {
+    val topic = Random.shuffle(topics).head
+    pubSubActor ! Publish(topic, "This is a message to a random topic " + topic)
+  }
 }
